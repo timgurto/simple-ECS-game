f423d2b6bea721152bb8429b2569debfc23f76ed
Tim Gurto <t_gurto@wargaming.net>
2022-08-23 11:27:00 +1000

    Make it a minimal game

--- ecs2/main.cpp
+++ ecs2/main.cpp
@@ -2,9 +2,37 @@
 
 #include <iostream>
 
+auto playerLoc = 5;
+auto mapSize = 10;
+
+void handleInput() {
+  const auto LEFT_ARROW = 75;
+  const auto RIGHT_ARROW = 77;
+
+  switch (_getch()) {
+    case LEFT_ARROW:
+      --playerLoc;
+      break;
+
+    case RIGHT_ARROW:
+      ++playerLoc;
+      break;
+  }
+}
+
+void drawGameState() {
+  std::cout << '\r';
+
+  for (auto x = 1; x < playerLoc; ++x) std::cout << '.';
+  std::cout << 'P';
+  for (auto x = playerLoc + 1; x != mapSize; ++x) std::cout << '.';
+
+  std::cout << std::flush;
+}
+
 int main() {
   while (true) {
-    auto c = _getch();
-    std::cout << '\r' << c << std::flush;
+    drawGameState();
+    handleInput();
   }
 }
