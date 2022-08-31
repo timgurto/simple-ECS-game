e126043a67da7b6ed83ec953c5b2e495f532a3f1
Tim Gurto <timgurto@gmail.com>
2022-08-31 16:32:21 +1000

    Add explanatory example

--- ecs2/Component.h
+++ ecs2/Component.h
@@ -18,3 +18,25 @@ struct Component {
   }                              \
   ;                              \
   std::string ClassName::classID = #ClassName;
+
+/* Example:
+
+// Using macros:
+
+    BEGIN_COMPONENT(Cartesian)
+    int x;
+    int y;
+    END_COMPONENT(Cartesian)
+
+
+// Fully expanded:
+
+    struct Cartesian : public Component {
+        static std::string classID;
+        virtual std::string getClassID() const override { return classID; }
+
+        int x;
+        int y;
+    };
+    std::string Cartesian::classID = "Cartesian";
+*/
